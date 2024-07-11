d6d9da3288f5ada126ca16d75f92a0ac31cd0888
xvnoufal <noufal.vadakkayil@rakbank.ae>
2024-07-11 13:13:02 +0400

    setup projects

---
+++ src/routes.ts
@@ -0,0 +1,7 @@
+import { Application } from 'express';
+import { collapseTextChangeRangesAcrossMultipleVersions } from 'typescript';
+
+export default (app: Application) => {
+  const routes = () => {};
+  routes();
+};
